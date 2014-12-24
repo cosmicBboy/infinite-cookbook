3253c959ac54daa49d9c2911857f4ce96673648a
cosmicBboy <niels.bantilan@gmail.com>
2014-12-24 12:10:04 -0500

    added checkbox feature to ingredients list

--- infiniteCookbook/client/sidebar.js
+++ infiniteCookbook/client/sidebar.js
@@ -1,13 +1,13 @@
 //function to jump to template specified in element argument
-var scrollTo = function(offsetNum) {
-  $('.sidebar').animate(
-    { scrollTop: offsetNum },
-    { duration: 1000, easing: 'easeInOutCubic' }
-  );
-};
+// var scrollTo = function(offsetNum) {
+//   $('.sidebar').animate(
+//     { scrollTop: offsetNum },
+//     { duration: 1000, easing: 'easeInOutCubic' }
+//   );
+// };
 
 //declare id variable for recipe data to show on page
-var id, btmPadding, scrollStop;
+var id, ingredientsHeight, sidebarHeight;
 
 Template.sidebar.helpers({
 	recipeList: function() {
@@ -29,19 +29,10 @@ Template.sidebar.events({
 		console.log(this._id, id);
 		if (typeof id === 'undefined') {
 			Session.set('recipePageData', this._id);
-			if ( $('.title-text').offset().top > 0) {
-				setTimeout(function() {
-					scrollTo($(e.target).offset().top - 10);
-				}, 200);
-			}
+
 		} else if (id !== this._id) {
 			Session.set('recipePageData', this._id);
 		} else if (id === this._id) {
-			//scroll back to top
-			// scrollTo(0);
-			console.log($('.ingredients-container').height());
-			btmPadding = $('.ingredients-container').height();
-			$('ul.btn-group-vertical').css('padding-bottom', btmPadding)
 			Session.set('recipePageData', undefined);	
 		}
 	}
